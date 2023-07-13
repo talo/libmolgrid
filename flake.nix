38d94e35c700c6b857c1a9ac3c31ff209f6801ce
Ryan Swart <ryan@talosystems.com>
2023-07-13 15:11:47 +0800

    fix: compile without impure

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   description = "A very basic flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+  inputs.nixpkgs-base.url = "github:NixOS/nixpkgs/22.11";
+  inputs.nixpkgs.url = "github:numtide/nixpkgs-unfree/7331a9526557393edc2ff86d04ecd74b107f1b81";
+  inputs.nixpkgs.inputs.nixpkgs.follows = "nixpkgs-base";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nixpkgs-base }: {
 
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage (import ./default.nix) { };
 
