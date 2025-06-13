8c5c5f154d171d6b6b6292379fa82093edcb1b4b
Sean L <sean.laguna@qdx.co>
2025-06-13 01:18:33 -0500

    chore: format and clean up flake.nix & default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, cudaPackages
-, openbabel
-, zlib
-, boost
-, python3
-, python3Packages
-, pkg-config
+{
+  lib,
+  stdenv,
+  boost,
+  cmake,
+  cudaPackages,
+  openbabel,
+  pkg-config,
+  python3,
+  python3Packages,
+  zlib,
 }:
 
 stdenv.mkDerivation rec {
@@ -16,35 +16,24 @@ stdenv.mkDerivation rec {
   version = "master";
   src = ./.;
 
-  # fetchFromGitHub {
-  #   owner = "gnina";
-  #   repo = "libmolgrid";
-  #   rev = "v${version}";
-  #   hash = "sha256-YdEjXfrTf9hw0nMbC2JWZ7Gf/psZ4RQ6v6GUrx5yIoA=";
-  # };
-
-  #buildFlags = [ "-stdlib=libstdc++" ];
-
   nativeBuildInputs = [
     cmake
     pkg-config
   ];
 
   buildInputs = [
-    #gcc
+    boost.dev
     cudaPackages.cuda_cccl
     cudaPackages.cuda_cudart
     cudaPackages.cuda_nvcc
-    openbabel
-    #stdenv.cc.cc.lib
-    zlib
-    boost.dev
     python3
     python3Packages.boost
     python3Packages.numpy
     python3Packages.openbabel-bindings
     python3Packages.pyquaternion
     python3Packages.pytest
+    openbabel
+    zlib
   ];
 
   OPENBABEL3_INCLUDE_DIR = "${openbabel}/include/openbabel3";
@@ -55,10 +44,9 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with lib; {
-    description =
-      "Comprehensive library for fast, GPU accelerated molecular gridding for deep learning workflows";
+    description = "Comprehensive library for fast, GPU accelerated molecular gridding for deep learning workflows";
     homepage = "https://github.com/gnina/libmolgrid";
     license = licenses.asl20;
-    maintainers = with maintainers; [ ];
+    maintainers = [ ];
   };
 }
--- flake.nix
+++ flake.nix
@@ -5,16 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-      config.cudaSupport = true;
-      config.cudaCapabilities = ["7.0" "8.0" "8.6"];
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        config.cudaSupport = true;
+        config.cudaCapabilities = [
+          "7.0"
+          "8.0"
+          "8.6"
+        ];
+      };
+    in
+    {
+      packages.x86_64-linux.libmolgrid = pkgs.callPackage (import ./default.nix) { };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.libmolgrid;
     };
-  in
-  {
-    packages.x86_64-linux.default = pkgs.callPackage (import ./default.nix) { };
-  };
 }
